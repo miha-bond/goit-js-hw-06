3bc4d85d38b17239a5065c71ae3f82b7fbccba49
Miha Bondarenko <limon18@ukr.net>
2022-09-06 23:12:12 +0300

    add Task-03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,43 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listRef = document.querySelector('.gallery');
+console.log(listRef);
+const listGallary = images.map(({ url, alt }) => {
+  // const itamEl = document.createElement('li');
+  // const imgEl = document.createElement('img');
+  // imgEl.src = url;
+  // imgEl.alt = alt;
+  // imgEl.width = '700';
+  // itamEl.append(imgEl);
+  // listRef.append(itamEl);
+  // return listRef;
+  return `<li style = 'display: flex; flex-direction: column;align-items: center; margin: 20px;'><img src="${url}" alt="${alt}" width ='700'></li>`;
+});
+
+let markup = listGallary.join('');
+listRef.insertAdjacentHTML('afterend', markup);
+
+//? Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+
+//? <ul class="gallery"></ul>
+
+//! Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+//! Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+//! Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+//* const images = [
+//*   {
+//*     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//*     alt: "White and Black Long Fur Cat",
+//*   },
+//*   {
+//*     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//*     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//*   },
+//*   {
+//*     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//*     alt: "Group of Horses Running",
+//*   },
+//* ];
