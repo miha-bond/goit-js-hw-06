ef95f3a5d0eb94e27273c3da1e2a0aea11fea90d
Miha Bondarenko <limon18@ukr.net>
2022-09-08 12:53:35 +0300

    add Task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,23 @@
 const inputRef = document.querySelector('#validation-input');
 
-console.log(inputRef);
+const inoutBlur = event => {
+  const { dataset, value } = event.target;
+  const requiredLength = Number(dataset.length);
+  if (requiredLength === value.length) {
+    event.target.classList.add('valid');
+    changeClass('valid', 'invalid', event.target);
+  } else {
+    event.target.classList.add('invalid');
+    changeClass('invalid', 'valid', event.target);
+  }
+};
 
+function changeClass(add, remove, elem) {
+  elem.classList.add(add);
+  elem.classList.remove(remove);
+}
+
+inputRef.addEventListener('blur', inoutBlur);
 //! Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
 
 //*     <input
