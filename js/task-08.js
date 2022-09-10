639463c249fdeb5d1d59614670c79d1fdceb90b1
Miha Bondarenko <limon18@ukr.net>
2022-09-10 16:14:03 +0300

    add Task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,5 +1,43 @@
-//? Напиши скрипт управління формою логіна.
+const formRef = document.querySelector('.login-form');
+formRef.addEventListener('submit', onFormSubmit);
 
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formElements = event.currentTarget.elements;
+//   const email = formElements.email.value;
+//   const password = formElements.password.value;
+//   const formData = {
+//     email,
+//     password,
+//   };
+//   // const formData = new FormData(event.currentTatget)
+//   // formData.forEach((value, name )=>{
+//   // console.log(name);
+//   // console.log(value);
+//   // })
+//   event.currentTarget.reset();
+// }
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Всі поля повинні бути заповнені!');
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
+//? Напиши скрипт управління формою логіна.
+//! Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+//! Під час відправлення форми сторінка не повинна перезавантажуватися.
+//! Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+//! Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+//! Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 //*       <form class="login-form">
 //*         <label>
 //*           Email
@@ -11,9 +49,3 @@
 //*         </label>
 //*         <button type="submit">Login</button>
 //*       </form>
-
-//! Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
-//! Під час відправлення форми сторінка не повинна перезавантажуватися.
-//! Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
-//! Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
-//! Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
